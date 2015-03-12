01d64e6562258683fdac081ae93b341627a1c447
Morten Wollert Nygren <morten@wollert.org>
2015-03-12 21:20:36 +0100

    Reduced length of function names

--- utils.py
+++ utils.py
@@ -64,16 +64,21 @@ class Fan:
         self.p = GPIO.PWM(self.ctrl, 25000)
         self.p.start(self.dc)
 
-    def setDC(self, dc):
+    def set_DC(self, dc):
         if dc <= 100 and dc >= 0:
             self.dc = dc
         self.p.ChangeDutyCycle(self.dc)
         return dc
 
-    def changeDC(self, reldc):
-        self.reldc = reldc
-        return self.p.setDC(self.dc + self.reldc)
+    def setDC(self, dc):
+        return self.set_DC(dc)
 
+    def change_DC(self, reldc):
+        self.reldc = reldc
+        return self.p.set_DC(self.dc + self.reldc)
+
+    def changeDC(self, reldc):
+        return self.change_DC(reldc)
 
 # Get data from 1-wire temperature probes
 class Therm(object):
@@ -82,14 +87,17 @@ class Therm(object):
         self.adr = adr
         self.device_file = '/sys/bus/w1/devices/' + self.adr + '/w1_slave'
 
-    def read_temp_raw(self):
+    def read_raw(self):
         self.f = open(self.device_file, 'r')
         self.lines = self.f.readlines()
         self.f.close()
         return self.lines
 
-    def read_temp(self):
-        self.lines = self.read_temp_raw()
+    def read_temp_raw(self):
+        return self.read_raw()
+
+    def read(self):
+        self.lines = self.read_raw()
         while self.lines[0].strip()[-3:] != 'YES':
             time.sleep(0.2)
             self.lines = self.read_temp_raw()
@@ -99,13 +107,19 @@ class Therm(object):
             self.temp_c = float(self.temp_string) / 1000.0
             return self.temp_c
 
-    def store_temp(self):
+    def read_temp(self):
+        return self.read()
+
+    def store(self):
         self.temp = self.read_temp()
         db = MySQLdb.connect(user="beeruser",db="beerdb")
         c = db.cursor()
         c.execute("""UPDATE Temperature SET Value=%s, Time=now() where Address=%s""",(self.temp, self.adr))
         db.commit()
         return self.temp
+
+    def store_temp(self):
+        return self.store()
 
 # Returns the addresses of all connected 1-wire devices
 def get_1w_adr():
@@ -124,7 +138,7 @@ class Load(object):
         self.sps = 8
         self.load = 0        
 
-    def read_load(self, ch):
+    def read(self, ch):
         if ch == 1:
             self.load = self.adc.readADCDifferential(0, 1, self.pga, self.sps)
         if ch == 2:
@@ -133,7 +147,10 @@ class Load(object):
             pass
         return self.load
 
-    def store_load(self, ch):
+    def read_load(self, ch):
+        return self.read(ch) 
+
+    def store(self, ch):
         self.read_load(ch)
         db = MySQLdb.connect(user="beeruser",db="beerdb")
         c = db.cursor()
@@ -141,31 +158,34 @@ class Load(object):
         db.commit()
         return self.load
 
+    def store_load(self, ch):
+        return self.store(ch)
+
     def set_empty(self):
-        l1 = self.read_load(1)
-        l2 = self.read_load(2)
+        l1 = self.read(1)
+        l2 = self.read(2)
         db = MySQLdb.connect(user="beeruser",db="beerdb")
         c = db.cursor()
         c.execute("""UPDATE LoadCell SET Value=%s, Time=now() where ID=3""",(l1 + l2))
         db.commit()
-        return self.load
+        return (l1 + l2)
 
     def set_full(self):
-        l1 = self.read_load(1)
-        l2 = self.read_load(2)
+        l1 = self.read(1)
+        l2 = self.read(2)
         db = MySQLdb.connect(user="beeruser",db="beerdb")
         c = db.cursor()
         c.execute("""UPDATE LoadCell SET Value=%s, Time=now() where ID=4""",(l1 + l2))
         db.commit()
-        return self.load
+        return (l1 + l2)
 
     def set_missing(self):
-        l1 = self.read_load(1)
-        l2 = self.read_load(2)
+        l1 = self.read(1)
+        l2 = self.read(2)
         db = MySQLdb.connect(user="beeruser",db="beerdb")
         c = db.cursor()
         c.execute("""UPDATE LoadCell SET Value=%s, Time=now() where ID=5""",(l1 + l2))
         db.commit()
-        return self.load
+        return (l1 + l2)
 
 
